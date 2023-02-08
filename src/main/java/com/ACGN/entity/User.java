59405e691dc6d089693f8c14543ad3ead581a123
chenfs <1>
2023-02-08 16:18:09 +0800

    用户类

---
+++ src/main/java/com/ACGN/entity/User.java
@@ -0,0 +1,19 @@
+package com.ACGN.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String salt;   //密码盐值
+    private String email;
+    private int type;
+    private int status;   //是否激活
+    private String activationCode;  //激活码
+    private String headerUrl;
+    private Date createTime;
+}
